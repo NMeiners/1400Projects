05ee85d1b7afb0afec57cb7610607e6889b327cb
Brautigancody465 <Brautigancody@gmail.com>
2024-03-24 14:34:20 -0600

    fixed center window and frame size

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,18 +1,20 @@
+
 //imports
 import javax.swing.*;
 
-public class TrafficLight{
+public class TrafficLight {
     public static void start() {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                //frame and layout
+                // frame and layout
                 JFrame frame = new JFrame("Traffic Light Controller");
-                Listener listener = new Listener();//Must be included
-                frame.addWindowListener(listener);//Must be included
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//Must be included
-                frame.setSize(100, 100);
+                Listener listener = new Listener();// Must be included
+                frame.addWindowListener(listener);// Must be included
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// Must be included
+                frame.setSize(200, 200);
                 frame.setVisible(true);
+                frame.setLocationRelativeTo(null);
             }
         });
-    }//end start
-}//end class TrafficLight
+    }// end start
+}// end class TrafficLight
