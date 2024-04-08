635c4dd9920bdb8da37529f54e51f078ddc49d5c
Benjaminsisay <beniamamare23@gmail.com>
2024-04-08 10:20:44 -0600

    ben update

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,13 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
 
 public class Calculator {
     public static void start() {
@@ -9,10 +18,37 @@ public class Calculator {
                 Listener listener = new Listener();// Must be included
                 frame.addWindowListener(listener);// Must be included
                 frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// Must be included
+                JTextField display = new JTextField();
+                display.setEditable(false);
+                frame.add(display, BorderLayout.NORTH);
+                JPanel buttonPanel = new JPanel(new GridLayout(4, 4));
+                frame.add(buttonPanel, BorderLayout.CENTER);
+                String [] buttonLabels = {"7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"};
+                for(String label: buttonLabels){
+                    JButton button = new JButton(label);
+                    button.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e){
+                            String buttonText = button.getText();
+
+                            String currentText = display.getText();
+                            currentText += buttonText;
+                            
+                            display.setText(currentText);
+                        }
+                    });
+                    buttonPanel.add(button);
+                }
+
                 frame.setSize(600, 400);
                 frame.setVisible(true);
                 frame.setLocationRelativeTo(null);
             }
         });
     }// end start
+    public static void main(String[] args){
+        start();
+    }
 }// end Class Calculator
